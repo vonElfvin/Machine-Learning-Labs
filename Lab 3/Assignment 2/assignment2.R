7fbec5772b8aa054dab6a35175d9846f62eb81c5
vonElfvin <elvin.granat@gmail.com>
2017-12-12 11:59:48 +0100

    Done with Assignment 2

--- Lab 3/Assignment 2/assignment2.R
+++ Lab 3/Assignment 2/assignment2.R
@@ -6,8 +6,44 @@
 
 # Libraries
 library(neuralnet)
+set.seed(12345)
 
 # Setup
-stations.dataframe = read.csv("stations.csv")
+rads = runif(50, 0, 10)
+data = data.frame(rads, sin=sin(rads))
+n = dim(data)[1]
+ids = sample(1:n,n/2)
+training = data[ids,]
+validation = data[-ids,]
 
 # Functions
+
+mse = function(obs, pred){
+  return(mean((obs-pred)^2))
+}
+
+# Implementation
+set.seed(12345)
+weights.start = runif(50, -1, 1)
+mse.training = numeric(10)
+mse.validation = numeric(10)
+
+# Compute MSEs for each data set with different threshholds
+for(i in 1:10){
+  nn = neuralnet(sin~rads, training, hidden=10, threshold=i/1000, startweights=weights.start)
+  pred = compute(nn, validation$rads)
+  mse.training[i] = mse(training$sin, nn$result.matrix[1])
+  mse.validation[i] = mse(validation$sin, pred$net.result[1])
+}
+plot(mse.training, type="b", col="green", ylim=c(0.4,0.9))
+points(mse.validation, type="b", col="red")
+
+
+# Neural Network with best threshhold = 1
+nn = neuralnet(sin~rads, training, hidden=10, threshold=1/1000, startweights=weights.start)
+plot(nn)
+
+# Prediction
+prediction = prediction(nn)
+plot(prediction(nn)$rep1, col="blue")
+points(data, col="red")
